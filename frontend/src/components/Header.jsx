b2bbaa46363be4f9d086d122a290065bf4c6a8ef
Anup1603 <anupshaw1603@gmail.com>
2024-07-23 12:53:01 +0530

    logo init

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,8 +12,13 @@ function Header() {
     <>
       <header className="header">
         <div className="logo">
+          <img
+            src="./logo2.png"
+            alt="Today I Learned"
+            height="65px"
+            width="65px"
+          />
           <h1 className="title">@Tech-Byte</h1>
-          
         </div>
         <button className="btn btn-large" id="shareBtn" onClick={openCloseForm}>
           {showForm ? "CLOSE" : "SHARE A FACT"}
